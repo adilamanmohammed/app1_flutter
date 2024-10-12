61c5b9f5b9436df89fab4592d14a7958698733bd
adilamanmohammed <adil.mohammed@okstate.edu>
2024-10-12 12:12:41 -0700

    deleted the contains of main.dart

---
+++ first_app/ios/Flutter/flutter_export_environment 2.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/adilamanmohammed/Tools/Flutter SDK"
+export "FLUTTER_APPLICATION_PATH=/Users/adilamanmohammed/Documents/Projects/Flutter_Projects/app1_flutter/first_app"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/adilamanmohammed/Documents/Projects/Flutter_Projects/app1_flutter/first_app/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/adilamanmohammed/Documents/Projects/Flutter_Projects/app1_flutter/first_app/.dart_tool/package_config.json"
